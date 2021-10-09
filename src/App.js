c7346d5c6ee226f012cf525240cc2781635ed616
Joseph <joseph@mindswarms.com>
2021-10-08 22:32:14 -0700

    MenuNav and SPages made SPA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import baseball from './static/baseball.png';
+import { Route, Switch, NavLink } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage/HomePage';
+import GamePage from './pages/GamePage/GamePage';
+import PlayerPage from './pages/PlayerPage/PlayerPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={baseball} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+           <li>
+             <NavLink to="/" exact={true} className="menu-item" activeStyle={{color: "#9146FF"}}>Home</NavLink>
+           </li>
+           <li>
+             <NavLink to="/Games" className="menu-item" activeStyle={{color: "#9146FF"}}>Games</NavLink>
+           </li>
+           <li>
+             <NavLink to="/Players" className="menu-item" activeStyle={{color: "#9146FF"}}>Players</NavLink>
+           </li>
+         </ul>
       </header>
+      <div className="App-body">
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/Games">
+            <GamePage />
+          </Route>
+          <Route path="/Players">
+            <PlayerPage />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
