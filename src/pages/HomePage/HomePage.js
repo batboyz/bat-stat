1b54d95f7bcdb3022047319759ea6258ff5d5d4d
Matthew <matthewsek30@gmail.com>
2021-10-14 12:47:29 -0700

    Added Balls and Strikes counter

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,63 @@
+import {useState} from "react";
 import baseball from '../../static/baseball.png';
 import './HomePage.scss';
 
 function HomePage() {
+    const[ballcount, setBall ] = useState(0);
+    const[strikecount, setStrike] = useState(0);
+    const[outcount, setOut] = useState(0);
+    const[inningcount, setInning] = useState(1);
+    const[halfcount, setHalf] = useState(true); // looks to see if in top of inning or bottom of inning
+  
+    const ballCounter = () => {
+        if (ballcount < 3 ) 
+            setBall(ballcount + 1); 
+        else{               
+            setBall(ballcount - ballcount);
+            setStrike(strikecount - strikecount);
+        }          
+    }
+
+    const strikeCounter = () => {
+        if (strikecount < 2)
+            setStrike(strikecount + 1);
+        else{
+            setStrike(strikecount - strikecount);
+            setBall(ballcount - ballcount);
+            outCounter();
+        }           
+    }
+
+    const outCounter = () => {
+        if(outcount < 2){    
+            setOut(outcount + 1);
+        }
+        else {
+            setOut(outcount - outcount)
+            if(halfcount === false ){
+                setInning(inningcount + 1)
+                setHalf(true);
+            }
+            else
+                setHalf(false);
+
+            
+        } 
+    }
+
+    let ballHit = () => {
+        setStrike(strikecount - strikecount)
+        setBall(ballcount - ballcount)
+
+    }
+
+    let ballHitOut = () => {
+        setStrike(strikecount - strikecount)
+        setBall(ballcount - ballcount)
+        outCounter();
+    }
+
+
     return (
         <div className="Homepage">
             <div className="Homepage-body">
@@ -15,6 +71,17 @@ function HomePage() {
                 >
                 React Documentation Here
                 </a>
+                <div className="counters">
+                <p> Balls: {ballcount} </p>
+                <button onClick={ballCounter} className='ballButton'>Ball</button>
+                <p> Strikes: {strikecount} </p>
+                <button onClick={strikeCounter} className='ballButton'> Strike</button>
+                <p> Outs: {outcount} </p>
+                <button onClick={ballHitOut} className='ballButton'> Out </button>
+                
+                <p> Inning: {inningcount} </p>
+                <button onClick={ballHit} className='ballButton'> Hit </button>
+                </div>
             </div>
         </div>
     );
